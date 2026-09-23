e27236958e2e22b2176fbd36f7c1b46e172266ca
Bishesh Amatya <bisheshamatya@Bisheshs-MacBook-Pro.local>
2026-09-23 20:46:04 +0200

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/bishesh-portfolio/',
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false
-    }
-  }
 })
