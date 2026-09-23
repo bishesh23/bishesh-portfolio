0b927391041cc9a71949a235cf84b832c4c1c660
Bishesh Amatya <bisheshamatya@Bisheshs-MacBook-Pro.local>
2026-09-23 19:52:17 +0200

    Updated portfolio

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import './Hero.css'
-import IMG_1027 from '../../assets/IMG_1027.jpg'
+import profileimage from '../../assets/IMG_1027.jpg'
 import AnchorLink from "react-anchor-link-smooth-scroll";
 const Hero = () => {
     return (
         <div id='home' className="hero">
-            <img src={IMG_1027} />
+            <img src={profileimage} />
             <h1> <span>I am Bishesh Amatya,</span> student at University of Bamberg </h1>
             <p> I am from Nepal and live in a beautiful city called Bamberg. </p>
             <div className="hero-action">
